ce37073f4ea80f84567f00d7d9a9e787cdc6dca3
dominic-wassef <domwassef@gmail.com>
2022-12-23 16:57:32 -0500

    chore: inserting host into db

---
+++ internal/repository/dbrepo/host_postgresql.go
@@ -0,0 +1,36 @@
+package dbrepo
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/dominic-wassef/gopher-runner/internal/models"
+)
+
+// inserts a host into the database
+func (m *postgresDBRepo) InsertHost(h models.Host) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	query := `insert into hosts (host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at)
+		values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning id`
+
+	var newID int
+	err := m.DB.QueryRowContext(ctx, query,
+		h.HostName,
+		h.CanonicalName,
+		h.URL,
+		h.IP,
+		h.IPV6,
+		h.Location,
+		h.OS,
+		h.Active,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+	if err != nil {
+		log.Println(err)
+		return newID, err
+	}
+	return newID, nil
+}
