a8acdb9b748f0b15d529059dc310d4f2a65de9c4
dominic-wassef <domwassef@gmail.com>
2022-12-24 23:30:20 -0500

    Adding: GetHostByID & UpdateHost func to handle DB

--- internal/repository/dbrepo/host_postgresql.go
+++ internal/repository/dbrepo/host_postgresql.go
@@ -8,14 +8,16 @@ import (
 	"github.com/dominic-wassef/gopher-runner/internal/models"
 )
 
-// inserts a host into the database
+// InsertHost inserts a host into the database
 func (m *postgresDBRepo) InsertHost(h models.Host) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
 	query := `insert into hosts (host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at)
-		values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning id`
+				values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning id`
 
 	var newID int
+
 	err := m.DB.QueryRowContext(ctx, query,
 		h.HostName,
 		h.CanonicalName,
@@ -28,9 +30,77 @@ func (m *postgresDBRepo) InsertHost(h models.Host) (int, error) {
 		time.Now(),
 		time.Now(),
 	).Scan(&newID)
+
 	if err != nil {
 		log.Println(err)
 		return newID, err
 	}
+
 	return newID, nil
+}
+
+// GetHostByID gets a host by id and returns models.Host
+func (m *postgresDBRepo) GetHostByID(id int) (models.Host, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		select id, host_name, canonical_name, url, ip, ipv6, location, os, active, created_at, updated_at
+		from hosts where id = $1
+`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var h models.Host
+
+	err := row.Scan(
+		&h.ID,
+		&h.HostName,
+		&h.CanonicalName,
+		&h.URL,
+		&h.IP,
+		&h.IPV6,
+		&h.Location,
+		&h.OS,
+		&h.Active,
+		&h.CreatedAt,
+		&h.UpdatedAt,
+	)
+
+	if err != nil {
+		return h, err
+	}
+
+	return h, nil
+}
+
+// UpdateHost updates a host in the database
+func (m *postgresDBRepo) UpdateHost(h models.Host) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		update hosts set host_name = $1, canonical_name = $2, url = $3, ip = $4, ipv6 = $5, os = $6
+		active = $7, location = $8, updated_at = $9 where id = $10
+`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		h.HostName,
+		h.CanonicalName,
+		h.URL,
+		h.IP,
+		h.IPV6,
+		h.OS,
+		h.Active,
+		h.Location,
+		time.Now(),
+		h.ID,
+	)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
 }
