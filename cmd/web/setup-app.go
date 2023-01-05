49e807fd468c6a739366c681c1cce82d21df18c4
dominic-wassef <domwassef@gmail.com>
2023-01-05 17:44:31 -0500

    chore: add monitoring to setup

--- cmd/web/setup-app.go
+++ cmd/web/setup-app.go
@@ -16,6 +16,7 @@ import (
 	"github.com/dominic-wassef/gopher-runner/internal/handlers"
 	"github.com/dominic-wassef/gopher-runner/internal/helpers"
 	"github.com/pusher/pusher-http-go"
+	"github.com/robfig/cron/v3"
 )
 
 func setupApp() (*string, error) {
@@ -138,6 +139,20 @@ func setupApp() (*string, error) {
 	log.Println("Secure", *pusherSecure)
 
 	app.WsClient = wsClient
+
+	monitorMap := make(map[int]cron.EntryID)
+	app.MonitorMap = monitorMap
+
+	localZone, _ := time.LoadLocation("Local")
+	scheduler := cron.New(cron.WithLocation(localZone), cron.WithChain(
+		cron.DelayIfStillRunning(cron.DefaultLogger),
+		cron.Recover(cron.DefaultLogger),
+	))
+
+	app.Scheduler = scheduler
+	app.Scheduler.Start()
+
+	startMonitoring()
 
 	helpers.NewHelpers(&app)
 
