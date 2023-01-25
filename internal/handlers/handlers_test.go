e42b25e64f7a3afba5e947dee7c1237515a727d9
dominic-wassef <domwassef@gmail.com>
2023-01-24 23:48:16 -0500

    test: pusherAuth

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -63,5 +64,41 @@ func TestLoginScreen(t *testing.T) {
 				t.Errorf("%s, expected %d, but got %d", e.name, e.expectedResponseCode, rr.Code)
 			}
 		}
+	}
+}
+
+func TestDBRepo_PusherAuth(t *testing.T) {
+	postedData := url.Values{
+		"socket_id":    {"1359961149.1836558958"},
+		"channel_name": {"private-channel-1"},
+	}
+
+	req, _ := http.NewRequest("POST", "/pusher/auth", strings.NewReader(postedData.Encode()))
+
+	ctx := getCtx(req)
+	req = req.WithContext(ctx)
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(Repo.PusherAuth)
+
+	handler.ServeHTTP(rr, req)
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected response 200 but got %d", rr.Code)
+	}
+
+	type pusherResp struct {
+		Auth string `json:"auth"`
+	}
+
+	var p pusherResp
+
+	err := json.NewDecoder(rr.Body).Decode(&p)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(p.Auth) == 0 {
+		t.Error("Empty JSON response!")
 	}
 }
