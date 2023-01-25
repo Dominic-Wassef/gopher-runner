da5bc059c097724b57ee988035123d1a0c588e87
dominic-wassef <domwassef@gmail.com>
2023-01-24 23:48:44 -0500

    test: websockets

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -21,6 +21,7 @@ import (
 var testSession *scs.SessionManager
 
 func TestMain(m *testing.M) {
+
 	testSession = scs.New()
 	testSession.Lifetime = 24 * time.Hour
 	testSession.Cookie.Persist = true
@@ -29,6 +30,7 @@ func TestMain(m *testing.M) {
 
 	mailQueue := make(chan channeldata.MailJob, 5)
 
+	// define application configuration
 	a := config.AppConfig{
 		DB:           &driver.DB{},
 		Session:      testSession,
@@ -40,9 +42,11 @@ func TestMain(m *testing.M) {
 	app = &a
 
 	preferenceMap := make(map[string]string)
+
 	app.PreferenceMap = preferenceMap
 
-	wsClient := pusher.Client{
+	// create pusher client
+	dws := dummyWs{
 		AppID:  "1",
 		Secret: "123abc",
 		Key:    "abc123",
@@ -50,7 +54,7 @@ func TestMain(m *testing.M) {
 		Host:   "localhost:4001",
 	}
 
-	app.WsClient = wsClient
+	app.WsClient = &dws
 
 	monitorMap := make(map[int]cron.EntryID)
 	app.MonitorMap = monitorMap
@@ -73,6 +77,7 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+// gets the context with session added
 func getCtx(req *http.Request) context.Context {
 	ctx, err := testSession.Load(req.Context(), req.Header.Get("X-Session"))
 	if err != nil {
@@ -81,9 +86,75 @@ func getCtx(req *http.Request) context.Context {
 	return ctx
 }
 
+// NewTestHandlers creates a new repository
 func NewTestHandlers(a *config.AppConfig) *DBRepo {
 	return &DBRepo{
 		App: a,
 		DB:  dbrepo.NewTestingRepo(a),
 	}
+}
+
+// dummyWs is a type that satisfies the pusher.Client interface
+type dummyWs struct {
+	AppID                        string
+	Key                          string
+	Secret                       string
+	Host                         string // host or host:port pair
+	Secure                       bool   // true for HTTPS
+	Cluster                      string
+	HTTPClient                   *http.Client
+	EncryptionMasterKey          string  // deprecated
+	EncryptionMasterKeyBase64    string  // for E2E
+	validatedEncryptionMasterKey *[]byte // parsed key for use
+}
+
+func (c *dummyWs) Trigger(channel string, eventName string, data interface{}) error {
+	return nil
+}
+
+func (c *dummyWs) TriggerMulti(channels []string, eventName string, data interface{}) error {
+	return nil
+}
+
+func (c *dummyWs) TriggerExclusive(channel string, eventName string, data interface{}, socketID string) error {
+	return nil
+}
+
+func (c *dummyWs) TriggerMultiExclusive(channels []string, eventName string, data interface{}, socketID string) error {
+	return nil
+}
+
+func (c *dummyWs) TriggerBatch(batch []pusher.Event) error {
+	return nil
+}
+
+func (c *dummyWs) Channels(additionalQueries map[string]string) (*pusher.ChannelsList, error) {
+	var cl pusher.ChannelsList
+	return &cl, nil
+}
+
+func (c *dummyWs) Channel(name string, additionalQueries map[string]string) (*pusher.Channel, error) {
+	var cl pusher.Channel
+	return &cl, nil
+}
+
+func (c *dummyWs) GetChannelUsers(name string) (*pusher.Users, error) {
+	var cl pusher.Users
+	return &cl, nil
+}
+
+func (c *dummyWs) AuthenticatePrivateChannel(params []byte) (response []byte, err error) {
+	return []byte("Hello"), nil
+}
+
+func (c *dummyWs) AuthenticatePresenceChannel(params []byte, member pusher.MemberData) (response []byte, err error) {
+	jStr := `
+	{"auth":"abc123:b75c9f83f1a1dbe7d6933316348039c6270b27a416286385a0fed98529cf46d1","channel_data":"{\"user_id\":\"1\",\"user_info\":{\"id\":\"1\",\"name\":\"Admin\"}}"}
+`
+	return []byte(jStr), nil
+}
+
+func (c *dummyWs) Webhook(header http.Header, body []byte) (*pusher.Webhook, error) {
+	var wh pusher.Webhook
+	return &wh, nil
 }
